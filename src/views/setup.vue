5b0b627bd4eba7de6fab667f8e14f960ee35a841
Phil Dale <dalephilip@gmail.com>
2021-02-07 08:08:57 +0000

    fix score bug

--- src/views/setup.vue
+++ src/views/setup.vue
@@ -128,7 +128,7 @@ export default {
         return this.$store.getters.pointsForWin;
       },
       set: function (newValue) {
-        this.$store.commit("pointsForWin", newValue);
+        this.$store.commit("pointsForWin", parseInt(newValue));
       },
     },
     vowelCost: {
